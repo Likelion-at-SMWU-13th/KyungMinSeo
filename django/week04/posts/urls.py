b148700fa08fbe9e6315289ee09c2d639fc0dcf6
mxinseo <kyungminseo10@gmail.com>
2025-05-20 14:11:29 +0900

    Fix : 오타 정정

--- django/week04/posts/urls.py
+++ django/week04/posts/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 
 from .views import PostModelViewSet
 
-app_names="posts"
+app_name="posts"
 
 router_post = routers.DefaultRouter()
 router_post.register('', PostModelViewSet)
@@ -13,3 +13,4 @@ urlpatterns = [
     path('', include(router_post.urls)),
     path('admin/', admin.site.urls)
 ]
+
